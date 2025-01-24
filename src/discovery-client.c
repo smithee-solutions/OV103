e531793554e9fe02094984ea0c2a497010b63c46
Rodney Thayer <github@smithee.solutions>
2025-01-24 02:44:11 +0000

    sketch client

--- src/discovery-client.c
+++ src/discovery-client.c
@@ -2,6 +2,23 @@ int main
   (int argc,
   char *argv [])
 {
+#if 0
+  init to undiscovered
+  read last known pd address (optional)
+
+  wait for a command
+  if it's not config address
+    if it's not for me
+      ignore it
+    else
+      nak it (optional?)
+  else /* it is config address */
+    if it's not a discovery command nak it (optional?)
+    else
+      if it's start-discover do nothing, log it
+      if it's discover then random backoff and respond
+      if it's discovery-set then set address, ack, leave undiscovered mode, output config, stop
+#endif
   return(0);
 }
 
